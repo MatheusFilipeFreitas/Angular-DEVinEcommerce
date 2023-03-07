9715dc9914d45e1079fbb787dbc10d87521cf21a
Math <mathffreitas@hotmail.com>
2023-03-07 16:16:42 -0300

    M1S10E02
    Desenvolva dois layouts para receber um modelo de dados de tela com menu e outra sem menu

--- Ecommerce/src/app/app.module.ts
+++ Ecommerce/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullscreenComponent } from './layouts/fullscreen/fullscreen.component';
+import { WithMenuComponent } from './layouts/with-menu/with-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullscreenComponent,
+    WithMenuComponent
   ],
   imports: [
     BrowserModule,
